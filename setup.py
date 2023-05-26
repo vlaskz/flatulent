17d12466e536ec752b19c027aa236d582096ee59
vlaskz <vlaskz@icloud.com>
2023-05-26 20:40:31 -0300

    fix: added 'find_packages()' to try to find the damn packages.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='flatulent',
     version='0.1.0',
     description='Flatulent é uma biblioteca de compressão que combina o algoritmo LZW com a codificação de Huffman '
-                'para obter taxas de compressão eficientes.',
+                'para obter taxas de compressão eficientes. Voltada para ambientes sem threading, como Lambda '
+                'Functions.',
     author='Isaias Velasquez',
     author_email='vlaskz@icloud.com',
     url='https://github.com/vlaskz/flatulent',
-    packages=['flatulent'],
+    packages=find_packages(),
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
